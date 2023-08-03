01a974c480e324cc89da62e0aeb21185778b9d5b
김대희 <0824dh@naver.com>
2023-08-03 13:47:58 +0900

    ♻️ ___FILEBASENAME___.swift

--- templates/TCA with ReducerProtocol.xctemplate/___FILEBASENAME___.swift
+++ templates/TCA with ReducerProtocol.xctemplate/___FILEBASENAME___.swift
@@ -1,14 +1,12 @@
-//___FILEHEADER___
-
 import ComposableArchitecture
 import Dependencies
 
-public struct ___VARIABLE_productName:identifier___: ReducerProtocol {
+public struct ___VARIABLE_productName:identifier___: Reducer {
   @Dependency(\.environment___VARIABLE_productName:identifier___) private var environment
 
   public init() {}
 
-  public var body: some ReducerProtocol<State, Action> {
+  public var body: some Reducer<State, Action> {
     BindingReducer()
     Reduce { state, action in
       switch action {
